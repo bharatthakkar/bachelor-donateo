214279ac6a75b4e6b32ee7cb4d2fd2a134f3b85b
sherine.hanna1992@gmail.com <sherine.hanna1992@gmail.com@db3fa360-fb53-5d2d-9608-1cecdee4c3e8>
2013-04-27 12:57:10 +0000

    added cases model

---
+++ Donateo/src/main/java/net/codejava/springmodels/Cases.java
@@ -0,0 +1,30 @@
+package net.codejava.springmodels;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cases extends Projects {
+	public Cases(int project_id, int ngo_id, String project_name,
+			String description, Date deadline, Date start_date, boolean done, boolean completed,
+			boolean volunteer, boolean donate_money, boolean donate_object,
+			int collected_amount, int amount, int urgency_id) {
+				super(project_id, ngo_id, project_name, description, deadline, start_date, done, volunteer, donate_money, donate_object, collected_amount, amount, urgency_id);
+				super.setProject_id(project_id);
+				super.setNgo_id(ngo_id);
+				super.setProject_name(project_name);
+				super.setDescription(description);
+				super.setDeadline(deadline);
+				super.setStart_date(start_date);
+				super.setDone(done);
+				super.setCompleted(completed);
+				super.setVolunteer(volunteer);
+				super.setDonate_money(donate_money);
+				super.setDonate_object(donate_object);
+				super.setCollected_amount(collected_amount);
+				super.setAmount(amount);
+				super.setUrgency_id(urgency_id);
+				
+		// TODO Auto-generated constructor stub
+	}
+
+}
