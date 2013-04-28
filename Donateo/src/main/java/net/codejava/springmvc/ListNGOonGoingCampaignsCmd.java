eb4c22ddc29c7f53e5c16fb5a4d1e66e0841eb28
mirna.elbendary@gmail.com <mirna.elbendary@gmail.com@db3fa360-fb53-5d2d-9608-1cecdee4c3e8>
2013-04-28 00:48:11 +0000

    added ListNGOonGoingCampaignsCmd.java

---
+++ Donateo/src/main/java/net/codejava/springmvc/ListNGOonGoingCampaignsCmd.java
@@ -0,0 +1,117 @@
+package net.codejava.springmvc;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import net.codejava.springmodels.Cases;
+import net.codejava.springmodels.Crowdfundings;
+import net.codejava.springmodels.Projects;
+
+public class ListNGOonGoingCampaignsCmd implements Command {
+
+	@Override
+	public Hashtable execute(Hashtable htblInputParams) {
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = null;
+
+			try {
+				Hashtable<String, Object> result = new Hashtable<String, Object>();
+
+				conn = DriverManager
+						.getConnection(
+								"jdbc:mysql://localhost:3306/DonationCrowdfundingProject",
+								"orange", "");
+
+				Set set = htblInputParams.entrySet();
+				Iterator it = set.iterator();
+				Map.Entry entry = (Map.Entry) it.next();
+
+				int ngo_id = (Integer) entry.getValue();
+
+				CallableStatement listOnGoingCampaigns = conn
+						.prepareCall("{call listOnGoingCampaigns(? , ?)}");
+
+				listOnGoingCampaigns.setInt("NGO_id", ngo_id);
+				listOnGoingCampaigns.registerOutParameter(2, Types.VARCHAR); // success
+
+				ResultSet rs = null;
+				rs = listOnGoingCampaigns.executeQuery();
+				ArrayList<Crowdfundings> projects = new ArrayList<Crowdfundings>();
+				System.out.println("output message is: "
+						+ listOnGoingCampaigns.getString(2));
+
+				if (listOnGoingCampaigns.getString("output_msg").equals("")
+						|| listOnGoingCampaigns.getString("output_msg") == null) { // success
+
+					while (rs.next()) {
+						System.out.println("I have results in rs");
+
+						projects.add(new Crowdfundings(rs.getInt("project_id"),
+								rs.getInt("ngo_id"), rs
+										.getString("project_name"), rs
+										.getString("description"), rs
+										.getDate("deadline"), rs
+										.getDate("start_date"), rs
+										.getBoolean("done"), rs
+										.getBoolean("completed"), rs
+										.getBoolean("volunteer"), rs
+										.getBoolean("donate_money"), rs
+										.getBoolean("donate_object"), rs
+										.getInt("collected_amount"), rs
+										.getInt("amount"), rs
+										.getInt("urgency_id"), rs
+										.getBoolean("verified")));
+					}
+
+					result.put("listOnGoingCampaigns", projects);
+					Set entrySet = result.entrySet();
+					Iterator itr = entrySet.iterator();
+					System.out.println("Hashtable entries : ");
+					while (itr.hasNext())
+						System.out.println(itr.next());
+					return result;
+				} else if (listOnGoingCampaigns.getString("output_msg").equals(
+						"No on-going campaigns")) { // failure
+					result.put("listOnGoingCampaigns", "There are no ongoing Campaigns.");
+					System.out.println("There are no ongoing Campaigns.");
+					return result;
+				}
+
+			} catch (SQLException e) {
+				System.out.println("SQLException: " + e.getMessage());
+				System.out.println("ErrorCode: " + e.getErrorCode());
+				e.printStackTrace();
+			}
+
+		}
+
+		catch (ClassNotFoundException e) {
+
+			System.out.println("Class not found exception");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		ListNGOonGoingCampaignsCmd list = new ListNGOonGoingCampaignsCmd();
+		Hashtable input = new Hashtable();
+		input.put("ngo_id", 1);
+		ListNGOonGoingCampaignsCmd l = new ListNGOonGoingCampaignsCmd();
+		Hashtable out = l.execute(input);
+		System.out.println("end of main");
+	}
+
+}
